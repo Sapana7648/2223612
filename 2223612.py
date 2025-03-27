f7fd0dbbbcd4eb1bf2d3094e7fac021a83ebd52e
Sapana7648 <sapanahipparkar1@gmail.com>
2025-03-27 16:31:15 +0530

    Create 2223612.py

---
+++ 2223612.py
@@ -0,0 +1,17 @@
+def run(path) :     
+    # import modules here 
+    import numpy as np
+    import pandas as pd 
+    import matplotlib as pltplot 
+
+
+    # logic 
+    df = pd.read_excel("D:\roll_no.py")
+    df= df_id int, df_name varchar(20), absent_start_date date, absent_end_date date, total_absent_days int
+    df.head()
+    df()
+    
+    
+    # return your output
+    return df
+
